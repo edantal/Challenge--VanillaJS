3a9ed69b4702e6bd4210d91002fe6eb6caecf8a7
edanTal <edant81@gmail.com>
2024-04-26 16:34:33 +0200

    Add local file for quotes

---
+++ movie-quotes/quotes.js
@@ -0,0 +1,142 @@
+const localQuotes = [
+  {
+    text: 'May the Force be with you.',
+    author: 'Star Wars, 1977',
+  },
+  {
+    text: "There's no place like home.",
+    author: 'The Wizard of Oz, 1939',
+  },
+  {
+    text: "I'm the king of the world!",
+    author: 'Titanic, 1997',
+  },
+  {
+    text: 'Elementary, my dear Watson.',
+    author: 'The Adventures of Sherlock Holmes, 1939',
+  },
+  {
+    text: "It's alive! It's alive!",
+    author: 'Frankenstein, 1931',
+  },
+  {
+    text: "My mama always said life was like a box of chocolates. You never know what you're gonna get.",
+    author: 'Forrest Gump, 1994',
+  },
+  {
+    text: "I'll be back.",
+    author: 'The Terminator, 1984',
+  },
+  {
+    text: "You're gonna need a bigger boat.",
+    author: 'Jaws, 1975',
+  },
+  {
+    text: 'My precious.',
+    author: 'The Lord of the Rings: Two Towers, 2002',
+  },
+  {
+    text: 'Houston, we have a problem.',
+    author: 'Apollo 13, 1995',
+  },
+  {
+    text: 'E.T. phone home.',
+    author: 'E.T. the Extra-Terrestrial, 1982',
+  },
+  {
+    text: "You can't handle the truth!",
+    author: 'A Few Good Men, 1992',
+  },
+  {
+    text: 'A martini. Shaken, not stirred.',
+    author: 'Goldfinger, 1964',
+  },
+  {
+    text: 'If you build it, he will come.',
+    author: 'Field of Dreams, 1989',
+  },
+  {
+    text: 'The stuff that dreams are made of.',
+    author: 'The Maltese Falcon, 1941',
+  },
+  {
+    text: 'Keep your friends close, but your enemies closer.',
+    author: 'The Godfather Part II, 1974',
+  },
+  {
+    text: 'I am your father.',
+    author: 'Star Wars Episode V: The Empire Strikes Back, 1980',
+  },
+  {
+    text: 'Just keep swimming.',
+    author: 'Finding Nemo, 2003',
+  },
+  {
+    text: 'Hasta la vista, baby.',
+    author: 'erminator 2: Judgment Day, 1991',
+  },
+  {
+    text: 'Bond. James Bond.',
+    author: 'Dr. No, 1962',
+  },
+  {
+    text: 'You talking to me?',
+    author: 'Taxi Driver, 1976',
+  },
+  {
+    text: "Roads? Where we're going we don't need roads.",
+    author: 'Back to the Future, 1985',
+  },
+  {
+    text: "That'll do, pig. That'll do.",
+    author: 'Babe, 1995',
+  },
+  {
+    text: "I'm walking here! I'm walking here!",
+    author: 'Midnight Cowboy, 1969',
+  },
+  {
+    text: 'It was beauty killed the beast.',
+    author: 'King Kong, 1933',
+  },
+  {
+    text: 'Stella! Hey, Stella!',
+    author: 'A Streetcar Named Desire, 1951',
+  },
+  {
+    text: 'As if!',
+    author: 'Clueless, 1995',
+  },
+  {
+    text: "Here's Johnny!",
+    author: 'The Shining, 1980',
+  },
+  {
+    text: 'Rosebud.',
+    author: 'Citizen Kane, 1941',
+  },
+  {
+    text: "I'll have what she's having.",
+    author: 'When Harry met Sally, 1989',
+  },
+  {
+    text: 'Hello, my name is Inigo Montoya. You killed my father. Prepare to die.',
+    author: 'The Princess Bride, 1987',
+  },
+  {
+    text: 'Nobody puts Baby in a corner.',
+    author: 'Dirty Dancing, 1987',
+  },
+  {
+    text: "You had me at 'hello.'",
+    author: 'Jerry Maguire, 1996',
+  },
+  {
+    text: "Toto, I've a feeling we're not in Kansas anymore.",
+    author: 'The Wizard of Oz, 1939',
+  },
+  {
+    text: 'That rug really tied the room together.',
+    author: 'The Big Lebowski, 1998',
+  },
+]
---
+++ movie-quotes/script.js
@@ -0,0 +1,75 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+let apiQuotes = []
+
+const loading = () => {
+  loader.hidden = false
+  quoteContainer.hidden = true
+}
+
+const loadingComplete = () => {
+  loader.hidden = true
+  quoteContainer.hidden = false
+}
+
+const newQuoteRemote = () => {
+  loading()
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+
+  const isAuthor = quote.author !== 'type.fit'
+
+  authorText.textContent = isAuthor
+    ? quote.author.split(',').shift()
+    : 'Unknown'
+
+  quote.text.length > 50
+    ? quoteText.classList.add('quote__text--long')
+    : quoteText.classList.remove('quote__text--long')
+  quoteText.textContent = quote.text
+
+  loadingComplete()
+}
+
+const newQuoteLocal = () => {
+  loading()
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+
+  authorText.textContent = quote.author ? quote.author : 'Unknown'
+
+  quote.text.length > 50
+    ? quoteText.classList.add('quote__text--long')
+    : quoteText.classList.remove('quote__text--long')
+  quoteText.textContent = quote.text
+
+  loadingComplete()
+}
+
+const getQuotes = async () => {
+  loading()
+  const apiUrl = 'https://type.fit/api/quotes'
+
+  try {
+    const res = await fetch(apiUrl)
+    apiQuotes = await res.json()
+    newQuoteRemote()
+    console.log(apiQuotes)
+  } catch (err) {
+    // console.log(err)
+    newQuoteLocal()
+  }
+}
+
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+  window.open(twitterUrl, '_blank')
+}
+
+newQuoteBtn.addEventListener('click', getQuotes)
+twitterBtn.addEventListener('click', tweetQuote)
+
+getQuotes()
